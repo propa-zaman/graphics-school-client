6b2c3e636705f47a9c6d8cbc9e5773cd691b51cf
Afra <propazaman12@gmail.com>
2023-06-16 14:26:59 +0600

    available seats 0 condition working

--- src/pages/Classes/ClassTab.jsx
+++ src/pages/Classes/ClassTab.jsx
@@ -4,61 +4,61 @@ import { AuthContext } from '../../Providers/AuthProvider';
 import { useContext } from 'react';
 import useEnrolled from '../../hooks/useEnrolled';
 import { useLocation, useNavigate } from 'react-router-dom';
+import useAdmin from '../../hooks/useAdmin';
+import useInstructor from '../../hooks/useInstructor';
 
 const ClassTab = ({ school }) => {
   const { _id, class_image, class_name, instructor, available_seats, price } = school;
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const [, refetch] = useEnrolled();
   const navigate = useNavigate();
   const location = useLocation();
 
-//   TODO:make admin and instructors dynamic
-  const isAdmin = false;
-  const isInstructor = false;
+  const [isAdmin] = useAdmin();
+  const [isInstructor] = useInstructor();
 
-  const handleSelect = school=> {
+  const handleSelect = school => {
     console.log(school);
-    if(user && user.email){
-        const enrolledClass = {classId: _id, class_name, class_image, price, available_seats, email: user.email}
-        fetch('https://graphics-school-sever-propa-zaman.vercel.app/enrolled', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(enrolledClass)
-        })
+    if (user && user.email) {
+      const enrolledClass = { classId: _id, class_name, class_image, price, available_seats, email: user.email };
+      fetch('https://graphics-school-sever-propa-zaman.vercel.app/enrolled', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(enrolledClass)
+      })
         .then(res => res.json())
         .then(data => {
-            if(data.insertedId){
-                refetch(); // refetch enrolled to update the number of classes in the enrolled
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: 'Class added on the dashboard',
-                    showConfirmButton: false,
-                    timer: 1500
-                  })
-            }
-        })
+          if (data.insertedId) {
+            refetch(); // refetch enrolled to update the number of classes in the enrolled
+            Swal.fire({
+              position: 'top-end',
+              icon: 'success',
+              title: 'Class added on the dashboard',
+              showConfirmButton: false,
+              timer: 1500
+            });
+          }
+        });
+    } else {
+      Swal.fire({
+        title: 'Please login to enroll class',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Login now!'
+      }).then(result => {
+        if (result.isConfirmed) {
+          navigate('/login', { state: { from: location } });
+        }
+      });
     }
-    else{
-        Swal.fire({
-            title: 'Please login to enroll class',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Login now!'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              navigate('/login', {state: {from: location}})
-            }
-          })
-    }
-}
+  };
 
   return (
-    <div className={`flex items-center bg-white rounded-lg shadow-lg p-4 ${available_seats === 0 ? 'bg-red-200' : ''}`}>
+    <div className={`flex items-center bg-white rounded-lg shadow-lg p-4 ${available_seats <= 0 ? 'bg-red-300' : ''}`}>
       <div className="w-32 h-32 rounded-full overflow-hidden">
         <img src={class_image} alt="" className="object-cover w-full h-full" />
       </div>
